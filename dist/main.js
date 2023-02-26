7e1c0f10cc0a782c3c09096415921adc7338c4a4
Sankari Karthik <sankarikarthik03@gmail.com>
2023-02-26 14:05:36 +0530

    Create bare-bones homepage

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+console.log("Heya!");
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiSGV5YSFcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
